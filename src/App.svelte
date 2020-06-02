45fbab932134e3fcb4af8b15b7744a3ea29986d4
Enzo Daneri <enzodaneri@hotmail.com>
2020-06-02 14:25:33 -0300

    first-commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+
+
+
+<nav class="nav">
+<div class="nav-center">
+ <h1 class="nav-title">Budget Calculator</h1>
+
+</div>
+<button type="button" class="nav-btn">
+<i class="far fa-plus-square"></i>
+add item
+</button>
+</nav>
+<!--  https://github.com/EnzoDaneri/budget-calculator.git -->
